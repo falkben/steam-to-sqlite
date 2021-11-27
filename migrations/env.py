11165891c30b5f9eab69e34f9542ccf4085f7563
Ben Falk <falk.ben@gmail.com>
2021-11-27 16:07:00 -0500

    reset import order without isort

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
 from alembic import context
-from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
-from steam2sqlite.models import Genre
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,6 +20,7 @@ fileConfig(config.config_file_name)
 # target_metadata = mymodel.Base.metadata
 # target_metadata = None
 
+from steam2sqlite.models import Genre
 target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -65,7 +67,9 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
