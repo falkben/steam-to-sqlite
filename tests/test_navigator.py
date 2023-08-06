f5ed4c3b7b8e8b9f88e448ec8d461503cb16bd2b
Ben Falk <falk.ben@gmail.com>
2023-08-06 00:18:52 -0400

    update deps (#127)
    * update deps
    
    * reddit returning a weird 302 error so switch to another big site

--- tests/test_navigator.py
+++ tests/test_navigator.py
@@ -1,15 +1,20 @@
+from unittest.mock import patch
+
 import pytest
-from steam2sqlite.navigator import make_requests
+
+from steam2sqlite.navigator import NavigatorError, get, make_requests
 
 
+@patch.object(get, "__defaults__", (100, None))
 @pytest.mark.asyncio
 async def test_urls():
     urls = [
         "https://www.google.com",
         "https://www.amazon.com",
-        "https://www.reddit.com",
+        "https://www.youtube.com",
     ]
     responses = await make_requests(urls)
 
     for resp in responses:
+        assert not isinstance(resp, NavigatorError)
         assert resp.raise_for_status() is None
